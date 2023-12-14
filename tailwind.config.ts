89810b500b53dcbf0b17b566c879010078349403
FayeMao <fayemao68@gmail.com>
2023-12-14 14:07:36 -0500

    Add fallback metadata to layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        body: ["var(--font-nunito-sans"],
+        body: ["var(--font-nunito-sans)"],
         display: ["var(--font-nunito)"],
       }
     },
